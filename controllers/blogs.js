41ccbf2b7386a7683bbbdc98dc3dd6fa46551c8f
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-07-06 21:26:41 -0500

    4.2: Blog list, step 2

---
+++ controllers/blogs.js
@@ -0,0 +1,52 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (req, res) => {
+    Blog.find({}).then(blogs => {
+        res.json(blogs)
+    })
+})
+
+blogdRouter.get('/:id', (req, res, next) => {
+    Blog.findById(req.params.id)
+        .then(blog => {
+            if(blog) {
+                res.json(blog)
+            } else {
+                res.status(400).ned()
+            }
+        })
+        .catch(error => next(error))
+})
+
+blogsRouter.post('/', (req, res, next) => {
+    const body = req.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    })
+
+    blog.save().then(blogSaved => {
+        res.json(blogSaved)
+    }).catch(error => next(error))
+})
+
+blogsRouter.put('/:id', (req, res, next) => {
+    const body = req.body
+    const blog = {
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes
+    }
+
+    Blog.findByIdAndUpdate(req.params.id, blog, { new: true})
+        .then(updatedBlog => {
+            res.json(updatedBlog)
+        }).catch(error => next(error))
+})
+
+module.exports = blogsRouter
---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import globals from "globals";
+import js from '@eslint/js';
+import { defineConfig } from "eslint/config";
+import stylisticJs from "eslint-plugin-stylistic-js";
+
+
+export default defineConfig([
+  js.configs.recommended,
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { files: ["**/*.{js,mjs,cjs}"], 
+    languageOptions: { globals: globals.node, ecmaVersion: "latest", },
+    plugins: { "@stylistic/js": stylisticJs },
+    rules: {
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/linebreak-style": ["error", "unix"],
+      "@stylistic/js/quotes": ["error", "single"],
+      "@stylistic/js/semi": ["error", "never"],
+      eqeqeq: "error",
+      "no-trailing-spaces": "error",
+      "object-curly-spacing": ["error", "always"],
+      "comma-dangle": ["error", "always-multiline"],
+      "arrow-spacing": ["error", {before: true, after: true}],
+      "no-console": "off",
+    }
+  },
+  {
+    ignores: ["dist/**"]
+  }
+]);
