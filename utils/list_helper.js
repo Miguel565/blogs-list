3cb3eddfb664f8a02089f8b8cac56efa86d3b75f
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-07-08 19:31:23 -0500

    4.4: Helper functions and unit tests, step 2

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,4 +2,10 @@ const dummy = (blogs) => {
 	return 1;
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+	const likes = blogs.map(blog => blog.likes)
+	const sum = likes.reduce((acc, item) => acc + item, 0)
+	return sum
+}
+
+module.exports = { dummy, totalLikes }
