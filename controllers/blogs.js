cdcec79667806e453e0aa7cc0132dc8df80a01a9
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-07-22 17:02:45 -0500

    4.19: Blog list extensions, step 7

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,9 +1,11 @@
 const blogsRouter = require('express').Router()
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+    const blogs = await Blog.find({})
+        .populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
@@ -16,19 +18,33 @@ blogsRouter.get('/:id', async (request, response) => {
     }
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
+
 blogsRouter.post('/', async (request, response) => {
     if (!request.body.title || !request.body.url) {
         return response.status(400).json({ error: 'Title and URL are requestuired' })
     }
-
-    const user = await User.findById(request.body.userId)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'toekn invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
+    if (!user) {
+        return response.status(400).json({ error: 'UserId missing or not valid' })
+    }
 
     const blog = new Blog({
         title: request.body.title,
         author: request.body.author,
         url: request.body.url,
         likes: request.body.likes || 0,
-        user: user.id
+        user: user._id
     })
 
     const savedBlog = await blog.save()
