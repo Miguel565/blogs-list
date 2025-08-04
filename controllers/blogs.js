2d9f6d23d66f057a0da5d363493bfedfdb3c5164
Miguel Guerrero <miguel.guerrerog6@gmail.com>
2025-08-04 16:21:17 -0500

    Fixed file

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,7 +1,7 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
-const middleware = require('./utils/middleware')
+const middleware = require('../utils/middleware')
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
